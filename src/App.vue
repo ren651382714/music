71eef91e880b44840e5b535b3a906ca8c3a9a35c
ren651382714 <32952138+ren651382714@users.noreply.github.com>
2021-09-15 09:36:27 +0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+	<Head/>
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
@@ -6,8 +7,25 @@
   </div>
   <router-view/>
 </template>
-
+<script >
+	import Head from './views/head.vue';
+	export default {
+		name:'App',
+		components: {
+			Head
+		}
+	} 
+	
+</script>
 <style>
+*{
+	margin: 0;
+	padding: 0;
+	list-style-type: none;
+	text-decoration: none;
+	color: #333;
+  font-size: 16px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
