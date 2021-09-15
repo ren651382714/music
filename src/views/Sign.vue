bc8b73f8adf114f3397c1fb9ebc88f57c318c02a
ren651382714 <32952138+ren651382714@users.noreply.github.com>
2021-09-15 09:36:02 +0800

    Add files via upload

--- src/views/Sign.vue
+++ src/views/Sign.vue
@@ -18,23 +18,38 @@
 			const erweiimg = reactive({
 				img:''
 			});
+			/*登录状态*/
 			let Signin = async() => {
 				let dengru = await proxy.$axios.get('/login/status',{})
-				
-				
+				localStorage.setItem("myInfo",JSON.stringify(dengru.data.data))
+				console.log(dengru)
 				return dengru.data.data
-				this.$store.commit("setData", dengru.data.data)
-				console.log(dengru.data.data)
 			}
+			/*获取用户信息*/
+			let myInfo = async() => {
+				let arrs =  JSON.parse(localStorage.getItem('myInfo'))
+				let dengrus = await proxy.$axios.get(`/user/detail?uid=${arrs.profile.userId}`,{})
+					
+			}
+			/*获取账户信息*/
+			let zhInfo = async() => {
+				let arrs =  JSON.parse(localStorage.getItem('myInfo'))
+				let dengrus = await proxy.$axios.get('/user/account',{})
+				console.log(dengrus)
+				localStorage.setItem("myInfos",JSON.stringify(dengrus.data))	
+			}
+			/*事件*/
 			
+			
+			/*时间戳timestamp=*/
 			onMounted(async() =>{
-				let getkey = await proxy.$axios.get('/login/qr/key',{withCredentials: true})
-				
+				let getkey = await proxy.$axios.get('/login/qr/key?timestamp=New Date()',{withCredentials: true})
 				let getkeys = getkey.data.data.unikey
-				let getewimg = await proxy.$axios.get(`/login/qr/create?key=${getkeys}&qrimg=true`,{withCredentials: true})
+				let getewimg = await proxy.$axios.get(`/login/qr/create?key=${getkeys}&qrimg=true&timestamp=New Date()`,{withCredentials: true})
 				erweiimg.img = getewimg.data.data.qrimg
 				const checkStatus = async() => {
-					const res = await proxy.$axios.get(`/login/qr/check?key=${getkeys}`,{withCredentials: true})
+					const res = await proxy.$axios.get(`/login/qr/check?key=${getkeys}&timestamp=New Date()`,{withCredentials: true})
+					console.log(`查找cookies:${res}`)
 					return res.data
 					
 				}
@@ -46,27 +61,25 @@
 				            clearInterval(timer)
 				          }
 				          if (statusRes.code === 803) {
-				            
 				            clearInterval(timer)
 				            alert('授权登录成功')
 				            Signin()
-										
+										myInfo()
+										zhInfo()
+										clearInterval(timer)
 				          }
 				        }, 3000)
-								
-					
-				
 			})
 			
 			return{
-				erweiimg,Signin
+				erweiimg,Signin,myInfo,zhInfo
 			}
 		},
 		
 	}
 </script>
 
-<style>
+<style scope>
 	.el-row {
 			margin-bottom: 20px;
 			&:last-child {
